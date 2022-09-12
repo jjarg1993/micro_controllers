4177849b8e8690b3a19aa83fd67f76d9329cdd42
Juan Jose Arguello Guerra <juan.arguelloguerra@student.htw-berlin.de>
2022-09-12 22:22:20 +0200

    correct missing parenthesis

--- water_with_pico/pico_py/watering_system.py
+++ water_with_pico/pico_py/watering_system.py
@@ -1,38 +1,46 @@
 import machine
 import utime
+from machine import Pin
 
 adc_pico = machine.ADC(26)
-val = adc_pico.read_u16()
-motor = Pin(26, Pin.OUT)
-print(val)
+motor = Pin(22, Pin.OUT)
 trocken = 600
 feucht_genug = 350
 
-def average_humidity:
+
+def average_humidity():
     average = 0;
     for x in range(8):
-        average = adc_pico.read_u16()
+        average = average + adc_pico.read_u16()
+        utime.sleep(1)
     return average/8
+      
+print(average_humidity())
 
-def too_dry_average:
+def too_dry_average():
     if average_humidity()>trocken:
         return 1
     else:
         return 0
-def too_dry_once:
+    
+print(too_dry_average())
+
+def too_dry_once():
     humidity = adc_pico.read_u16()
     if humidity < feucht_genug:
         return 1
     else:
         return 0
 
-def water_plant:
+def water_plant():
     motor.value(1)
     utime.sleep(1)
     motor.value(0)
     utime.sleep(5)
     
-def watering_system:
+def watering_system():
     if too_dry_average() == 1:
         while too_dry_once():
             water_plant()
+
+watering_system()            
